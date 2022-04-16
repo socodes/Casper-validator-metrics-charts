f53d6a7adbea8b4b41652d9af1bbfb90ce9c1303
Muhammed Didin <mdidin80@gmail.com>
2022-04-16 18:48:31 +0300

    take amount of ram and commit step time of validator

--- main.R
+++ main.R
@@ -6,8 +6,40 @@ cat("\014")  # ctrl+L
 
 #import libraries
 library(httr)
+library(tidyverse)
+
+#function to parse amount of ram of validator.
+get_ram_amount <- function(str) {
+  location <- str_locate(str,'total_ram_bytes')
+  ram <- substr(str, location[2]+73, location[2]+83)
+  
+  sprintf("Total amount of RAM in validator: %s",ram)
+}
+
+#function to parse commit step time of validator.
+get_step_time <- function(str) {
+  step_location <- str_locate(str,'contract_runtime_latest_commit_step')
+  step <- substr(str, step_location[2]+140, step_location[2]+150)
+  
+  sprintf("Commit step time of validator: %s",step)
+}
 
 #get data with http request.
 result <- GET("http://65.21.235.219:8888/metrics")
+#parse content of result to char.
 content <- rawToChar(result$content)
-content
+#take ram amount
+get_ram_amount(content)
+#take commit step time
+get_step_time(content)
+
+
+
+
+
+
+
+
+
+
+
