af98a061949b84db0e4629c9dd9265f3b93e9739
Muhammed Didin <mdidin80@gmail.com>
2022-04-16 18:20:43 +0300

    take content of result and parse it to char

--- main.R
+++ main.R
@@ -4,6 +4,10 @@ rm(list = ls())
 # Clear current console
 cat("\014")  # ctrl+L
 
-
+#import libraries
 library(httr)
+
+#get data with http request.
 result <- GET("http://65.21.235.219:8888/metrics")
+content <- rawToChar(result$content)
+content
